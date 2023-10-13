b4adc4f0c87aaa68ef8b8f8edd2abca26b00c2a0
Sergio Utrera <sergiactive@gmail.com>
2023-10-13 11:33:26 +0100

    Completed Contact Form

--- inc/shortcodes/mb_heading.php
+++ inc/shortcodes/mb_heading.php
@@ -9,6 +9,23 @@ vc_map(array(
 		array(
 			'type' => 'textfield',
 			'admin_label' => true,
+			'heading' => 'Icon',
+			'description' => 'https://icons.getbootstrap.com/',
+			'param_name' => 'mb_heading_icon',
+		),
+		array(
+			'type' => 'checkbox',
+			'admin_label' => true,
+			'heading' => 'Rounded corners?',
+			'description' => 'Rounded corners',
+			'param_name' => 'mb_heading_rounded_corners',
+			'value' => array(
+				'Yes' => 1,
+			),
+		),
+		array(
+			'type' => 'textfield',
+			'admin_label' => true,
 			'heading' => 'Heading',
 			'description' => 'Heading',
 			'param_name' => 'mb_heading_heading',
@@ -33,7 +50,9 @@ vc_map(array(
 // mb vc shortcode
 add_shortcode('mb_heading', 'mb_heading');
 function mb_heading($atts, $content) {
-	extract(shortcode_atts(array(	
+	extract(shortcode_atts(array(
+		'mb_heading_icon' => '',
+		'mb_heading_rounded_corners' => '',
 		'mb_heading_heading' => '',
 		'mb_heading_subheading' => '',
 		'mb_heading_background_color' => '',
@@ -54,12 +73,15 @@ function mb_heading($atts, $content) {
 	$html .= '
 
 	<!-- Heading section -->
-		<section class="bg-light pt-5" id="mb-heading">
-			<div class="container px-5 mt-5">
+		<section class="'.($mb_heading_rounded_corners ? '' : 'bg-light'). 'pt-5" id="mb-heading">
+			<div class="container px-5'.($mb_heading_rounded_corners ? '' : ' mt-5').'">
+			'.($mb_heading_rounded_corners ? '<div class="bg-light rounded-3 py-5 px-4 px-md-5">' : '').'
 				<div class="text-center">
+				'.($mb_heading_icon ? '<div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi '.$mb_heading_icon.'"></i></div>' : '').'
 						<h1 class="fw-bolder">'.$mb_heading_heading.'</h1>
 						<p class="lead fw-normal text-muted mb-0">'.$mb_heading_subheading.'</p>
 				</div>
+				'.($mb_heading_rounded_corners ? '</div>' : '').'
 			</div>
 		</section>
 	';
--- inc/vc-elements.php
+++ inc/vc-elements.php
@@ -6,8 +6,8 @@ function mb_vc_elements() {
     require_once(dirname(__FILE__).'/shortcodes/mb_team_members.php');
     require_once(dirname(__FILE__).'/shortcodes/mb_heading.php');
     require_once(dirname(__FILE__).'/shortcodes/mb_pricing_section.php');
-    // require_once(dirname(__FILE__).'/shortcodes/mb_contact_cards.php');
-    // require_once(dirname(__FILE__).'/shortcodes/mb_contact_form.php');
+    require_once(dirname(__FILE__).'/shortcodes/mb_contact_cards.php');
+    require_once(dirname(__FILE__).'/shortcodes/mb_contact_form.php');
     // require_once(dirname(__FILE__).'/shortcodes/mb_faq_section.php');
     // require_once(dirname(__FILE__).'/shortcodes/mb_features_section.php');
     // require_once(dirname(__FILE__).'/shortcodes/mb_testimonial_section.php');
